81da86f663250df4ce0ef610cf7a006e34d3e24b
lgw <privateli@qq.com>
2019-12-26 17:05:58 +0800

    Site updated: 2019-12-26 17:05:36

--- js/main.js
+++ js/main.js
@@ -841,7 +841,7 @@ $(function () {
               var indexTitle = -1
               var indexContent = -1
               // only match artiles with not empty titles and contents
-              if (dataTitle !== '' && dataContent !== '') {
+              if (dataTitle !== '' /*&& dataContent !== ''*/) {
                 keywords.forEach(function (keyword, i) {
                   indexTitle = dataTitle.indexOf(keyword)
                   indexContent = dataContent.indexOf(keyword)
